52642e150cb00ca670bae7d8392be7cc8800ab88
jrlv16 <jl062705@sfr.fr>
2020-02-05 15:22:58 +0100

    settings pour envoi de mail via console

--- extrapi/settings.py
+++ extrapi/settings.py
@@ -144,13 +144,13 @@ REST_FRAMEWORK = {
     ],
 }
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anneg.lingerie@gmail.com'
-EMAIL_HOST_PASSWORD = 'nzwr hnfb rucr fhjn'
-EMAIL_USE_TLS = True 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 try:
     from .local_settings import *
