8181fa1269f3452de5f4365fa44cf9014f0a3d85
jrlv16 <jl062705@sfr.fr>
2020-02-05 14:51:28 +0100

    settings.py pour envoi email via google

--- extrapi/settings.py
+++ extrapi/settings.py
@@ -144,13 +144,13 @@ REST_FRAMEWORK = {
     ],
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'anneg.lingerie@gmail.com'
-# EMAIL_HOST_PASSWORD = 'culneaj65'
-# EMAIL_USE_TLS = True 
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'anneg.lingerie@gmail.com'
+EMAIL_HOST_PASSWORD = 'culneaj65'
+EMAIL_USE_TLS = True 
 
 try:
     from .local_settings import *
